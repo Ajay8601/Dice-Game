09d03661661c28194be06e66407a1ca3323b15ea
Ajay Kumar Vishwakarma <ajaykumarvishwakarma637@gmail.com>
2025-06-04 13:13:13 +0530

    many changes in code

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Dice-Game/', // ✅ Add correct base path here
+  base: './', // ✅ Add correct base path here
 })
